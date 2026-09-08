38842d64788eb9dfcd9632a56a85169d8aa93844
Varvara Moiseenko <moiseencoo@gmail.com>
2026-09-08 19:44:32 +0000

    (feat) add routing, pages, map

---
+++ src/app/pages/track/track.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../../header/header';
+
+@Component({
+  selector: 'app-track',
+  imports: [Header],
+  templateUrl: './track.html',
+  styleUrl: './track.css',
+})
+export class Track {
+
+}
